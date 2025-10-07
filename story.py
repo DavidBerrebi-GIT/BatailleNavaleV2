84eebd071c35193c7e379361f7226f7035afa45e
DavidBerrebi-GIT <berrebi.d.pro@gmail.com>
2025-10-07 08:54:38 +0200

    story

--- story.py
+++ story.py
@@ -9,5 +9,17 @@ boat1 = Boat(3, (4,4))
 boat2 = Boat(3, (6,3))
 board.put_boat(boat1)
 board.put_boat(boat2)
+
+
+
+board.shoot((4,4))
 board.show()
-board.show(False)
+
+board.shoot((4,5))
+board.shoot((4,6))
+board.show()
+
+board.shoot((0,0))
+board.show()
+
+
