c4e359ce27e57ff26aabf691cc6b27e4f321b470
DavidBerrebi-GIT <berrebi.d.pro@gmail.com>
2025-10-21 09:27:43 +0200

    graphics update

--- geaphicsTesting.py
+++ geaphicsTesting.py
@@ -4,5 +4,6 @@ from graphics import Graphics
 
 
 board = Board()
+board.put_random_all_boats()
 window= Graphics()
-window.draw_board()
+window.draw_board_player(board)
--- graphics.py
+++ graphics.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 class Graphics:
-    ColorChart = {}
+    ColorChart = {-1: "#fafafa", -2 : "#1658b4", -3:"#1658b4", -4 : "#ed8106", 0: "#23a418" }
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Bataille Navale")
@@ -11,10 +11,23 @@ class Graphics:
         self.canvas2 = tk.Canvas(self.root,width=1200, height=700, bg="white")
     
 
-    def draw_board(self, canvas, board):
+    def draw_board_player(self, board):
+        for i in range(10):
+            for j in range(10):
+                value = board.cells[i][j] 
+                if value >= 0:
+                    value = 0
+                fill =  Graphics.ColorChart[value]
+                self.canvas1.create_rectangle(2 + i*50, 2 + j*50, i*50 + 51 ,j*50 + 51 ,outline="black", fill=fill)
+        self.canvas1.pack()
+
+    def draw_board_opponnent(self, board):
         for i in range(10):
             for j in range(10):
                 value = board.cells[i][j]
+                if value >= 0:
+                    value = -1
                 fill =  Graphics.ColorChart[value]
-                canvas.create_rectangle(2 + i*50, 2 + j*50, i*50 + 51 ,j*50 + 51 ,outline="black", fill=fill)
-        canvas.pack()
+                self.canvas2.create_rectangle(2 + i*50, 2 + j*50, i*50 + 51 ,j*50 + 51 ,outline="black", fill=fill)
+        self.canvas2.pack()
+    
