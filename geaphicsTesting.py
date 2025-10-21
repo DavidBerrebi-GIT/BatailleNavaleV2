6ffdfc7258ff79901dfdf7b4bf45901de0bd91a3
DavidBerrebi-GIT <berrebi.d.pro@gmail.com>
2025-10-21 18:28:30 +0200

    Update geaphicsTesting.py

--- geaphicsTesting.py
+++ geaphicsTesting.py
@@ -1,7 +1,8 @@
 from board import Board
 from graphics import Graphics
+import random
 
-
+random.seed(0)
 
 board_player = Board()
 board_player.put_random_all_boats()
@@ -9,6 +10,9 @@ board_opponent = Board()
 board_opponent.put_random_all_boats()
 
 board_player.shoot((0,4))
+board_opponent.shoot((0,4))
+board_player.shoot((1,3))
+board_player.shoot((2,3))
 
 
 window= Graphics()
